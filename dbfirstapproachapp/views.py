6c723b5d074efeb80ee9167293407c8eed69b3c9
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-07-24 13:28:08 +0300

    Calculate running and sub total

--- dbfirstapproachapp/views.py
+++ dbfirstapproachapp/views.py
@@ -22,13 +22,75 @@ def GetEmployeeBasedOnRawQuery(request):
 
 #* is efficient way  using procedures
 def StoreProceduresDemo(request):
+    grandTotal = 0
+    runningTotal = 0
+    runningOrderTotal = 0
+    subtotal = 0
+    
     # procedureName ='USP_GetAllOrders'
     conn = Connection()
     cursor = conn.cursor()
     cursor.execute("{call USP_GetAllOrders}")
     orders = cursor.fetchall()
-    return render(request,'dbfirstapproach/showorders.html',{'orders':orders}) 
+    
+    # Calculate runningTotal,runningOrderTotal,grandTotal and Generate Three new Columns
+    newOrders =[]
+    # related with runningOrderTotal calculate
+    previousOrderID =0
+    for order in orders:
+        
+        grandTotal += order.BillAmount
+        runningTotal += order.BillAmount
 
+        # calculate runningOrderTotal
+        if previousOrderID == 0: 
+            previousOrderID = order.OrderID      
+            runningOrderTotal += order.BillAmount
+            subtotal +=order.BillAmount 
+                 
+        elif previousOrderID == order.OrderID:
+             runningOrderTotal += order.BillAmount
+             subtotal +=order.BillAmount
+        else:
+            newOrders.append(GenerateNewOrder(0,subtotal,0)) # To Calculate subTotal
+            previousOrderID = order.OrderID
+            runningOrderTotal = order.BillAmount
+            subtotal +=order.BillAmount 
+            
+        newOrders.append(GenerateNewOrder(order,runningTotal,runningOrderTotal))
+        
+    newOrders.append(GenerateNewOrder(0,subtotal,0))   # subtotal for last order
+    return render(request,'dbfirstapproach/showorders.html',{'orders':newOrders,'grandTotal':grandTotal}) 
+
+# Used To Generate order data in addition to two columns (runningTotal,runningOrderTotal)
+def GenerateNewOrder (order,runningTotal,runningOrderTotal):
+    if (order ==0):
+          newOrder = {
+            "OrderID":'',
+            "OrderDate":'',
+            "CompanyName":'',
+            "ProductName":'',
+            "UnitPrice":'',
+            "Quantity":'',
+            "BillAmount":'',
+            "RunningTotal":runningTotal,
+            "RunningOrderTotal":''
+        }
+    else:
+        newOrder = {
+            "OrderID":order.OrderID,
+            "OrderDate":order.OrderDate,
+            "CompanyName":order.CompanyName,
+            "ProductName":order.ProductName,
+            "UnitPrice":order.UnitPrice,
+            "Quantity":order.Quantity,
+            "BillAmount":order.BillAmount,
+            "RunningTotal":runningTotal,
+            "RunningOrderTotal":runningOrderTotal
+        }
+    return newOrder
+
+  
 def SPWithOutputParameter(request):
     # procedureName ='USP_GetAllOrders'
     conn = Connection()
@@ -43,6 +105,7 @@ def SPWithOutputParameter(request):
     
     return render(request,'dbfirstapproach/showorders.html',{'orders':orders,'counts':counts}) 
 
+# Connection to Database by pyodbc
 def Connection():
     
     ''' Related With Database Connection'''
